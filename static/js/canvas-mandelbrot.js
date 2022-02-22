1d2180b27d90999c7dbfc04eb2a0c60b0fc969aa
JAntonioVR <juanantoniovr98@gmail.com>
2022-02-22 16:21:54 +0100

    Añadido color (#10)
    Se añade código al shader para colorear el conjunto de Mandelbrot

--- static/js/canvas-mandelbrot.js
+++ static/js/canvas-mandelbrot.js
@@ -133,6 +133,10 @@ vec2 f(vec2 x, vec2 c) {
 	return mat2(x,-x.y,x.x)*x + c;
 }
 
+vec3 palette(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
+  return a + b*cos( 6.28318*(c*t+d) );
+}
+
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution;
   
@@ -140,19 +144,28 @@ void main() {
   vec2 c = u_zoomCenter + (uv * 4.0 - vec2(2.0)) * (u_zoomSize / 4.0);
   
   /* Now iterate the function. */
+  int iterations;
   vec2 z = vec2(0.0);
   bool escaped = false;
   for (int i = 0; i < 10000; i++) {
     /* Unfortunately, GLES 2 doesn't allow non-constant expressions in loop
        conditions so we have to do this ugly thing instead. */
     if (i > u_maxIterations) break;
+    iterations = i;
     z = f(z, c);
     if (length(z) > 2.0) {
       escaped = true;
       break;
     }
   }
-  gl_FragColor = escaped ? vec4(1.0) : vec4(vec3(0.0), 1.0);
+  gl_FragColor = escaped ? vec4(palette(
+    float(iterations)/ float(u_maxIterations), 
+    vec3(0.02, 0.02, 0.03), 
+    vec3(0.1, 0.2, 0.3), 
+    vec3(0.0, 0.3, 0.2), 
+    vec3(0.0, 0.5, 0.8)
+    ), 
+    1.0) : vec4(vec3(0.3, 0.5, 0.8), 1.0);
 }
 `;
 
